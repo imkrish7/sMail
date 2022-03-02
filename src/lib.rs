724014b447c144603ec3e65e4ec542284dbf5575
imkrish7 <singhkrishk07@gmail.com>
2022-03-02 14:11:32 +0530

    solana mail completed

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GNaVrTwdsGTBKmZA1rwVtXbPQur5yzsqTY4KRJBCkTq");
+
+pub mod entrypoint;
+pub mod error;
+pub mod instructions;
+pub mod processor;
+pub mod state;
